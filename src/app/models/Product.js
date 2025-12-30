b5464098e6103d5fa8b156eb82a4079507787ee0
Antonio <jorge_11000@hotmail.com>
2025-12-29 21:23:25 -0300

    verification if's same product

---
+++ src/app/models/Product.js
@@ -0,0 +1,31 @@
+import  { DataTypes, Model } from 'sequelize';
+
+class Product extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+       
+        name: {
+                type: DataTypes.STRING,
+                unique: true,
+              },
+        price:DataTypes.DECIMAL(10,2),
+        category:DataTypes.STRING,
+        path:DataTypes.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'products',
+        underscored: true,
+        timestamps: true,// garante created_at e updated_at automáticos
+      },
+
+     
+    );
+      
+    
+  }
+   
+}
+
+export default Product;
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,57 @@
+import * as Yup from 'yup'
+import Product from '../app/models/Product.js';
+
+
+
+
+class ProductController{
+    async store (req, res){
+        const schema = Yup.object({
+            name:Yup.string().required(),
+            price:Yup.number().required(),
+            category:Yup.string().required(),
+        });
+
+         try{
+            
+            schema.validateSync(req.body, {abortEarly: false} ); 
+        } catch(err){
+            return res.status(400).json({error: err.errors});/*passando o erro para o usuário, dessa forma aparece na tela o que está errado*/
+        }
+
+          const {name,price,category,path} = req.body;
+
+        const existProduct = await Product.findOne({
+            where:{
+                name
+            }
+        
+        })
+
+        if(existProduct){
+            return res.status(400).json({message:"Product already cadastrated!"})
+        }
+
+        const product = await Product.create({
+        name,
+        price,
+        category,
+        path,
+    });
+
+    return res.status(201).json({
+        Id: product.id,
+        name: product.name,
+        price: product.price,
+        category: product.category,
+        path:product.path,
+
+    });
+
+    
+
+
+    }
+}
+
+export default new ProductController();
