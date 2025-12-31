06ed0d43b3d1d8bb04972948ea80aaf84f565e92
Antonio <jorge_11000@hotmail.com>
2025-12-31 14:47:07 -0300

    create auth.js

---
+++ src/config/auth.js
@@ -0,0 +1,12 @@
+export default{
+    secret: 'fe39382918216e44b56ca743c19e4e15',
+    expiresIn:'7d',
+}
+
+
+// export default{} é igual a :
+// const config = {objeto} - export default config
+// o import fica normal
+
+
+//CRIAMOS ESSE ARQUIVO PARA DEIXAR A SECRET E O EXPIRES EM UM LOCAL SO ASSIM PODEMOS UTILIZAR DE FORMA MAIS FACIL EM OUTROS LUGARES
