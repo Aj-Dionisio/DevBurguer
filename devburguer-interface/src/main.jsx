54184dcd53348cea3f8cebf0b8fd694007e81179
Antonio <jorge_11000@hotmail.com>
2026-02-10 21:54:47 -0300

    criando globalstyles

--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { login } from './containers/Login'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {Login} from './containers/Login';
+import globalStyles from './styles/globalStyles';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1>
-      Login
-    </h1>
-  </StrictMode>,
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>    
+      <Login />
+      <globalStyles />  
+  </React.StrictMode>,
 )
