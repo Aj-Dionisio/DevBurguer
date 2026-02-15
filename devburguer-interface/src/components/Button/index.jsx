1a8d20d17032dda4f4ad6e4f9b1715207db15cbe
Antonio <jorge_11000@hotmail.com>
2026-02-15 14:09:00 -0300

    create a button as component

---
+++ devburguer-interface/src/components/Button/index.jsx
@@ -0,0 +1,15 @@
+import { ContainerButton } from "./styles";
+import PropTypes from "prop-types";
+
+
+
+export function Button ({children}){
+    return <ContainerButton>{children}</ContainerButton>
+
+}
+
+
+Button.propTypes ={
+    children: PropTypes.string,
+}
+
